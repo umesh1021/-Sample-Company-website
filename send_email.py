dfd61a2834e2d06a13bb4b685c54f15dbb9c8f5b
Umesh <urautgol20it@student.mes.ac.in>
2023-07-03 19:23:37 +0530

    completed

--- send_email.py
+++ send_email.py
@@ -5,12 +5,14 @@ def send_email(message):
     host = "smtp.gmail.com"
     port = 465
 
-    username = "app8flask@gmail.com"
-    password = "pmuuyjaxfejfpxkc"
+    username = "umeshroutgol.com"
+    password = "bavbahyledfzkekw"
 
-    receiver = "app8flask@gmail.com"
+    receiver = "umeshroutgol@gmail.com"
     context = ssl.create_default_context()
 
+    context=ssl.create_default_context()
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
         server.sendmail(username, receiver, message)
+
