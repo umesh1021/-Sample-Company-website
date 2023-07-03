52570e2e2d5933e634ebc11ee54e0186802bb28e
Umesh <urautgol20it@student.mes.ac.in>
2023-07-03 19:30:40 +0530

    adding email function in contact us through forms

--- send_email.py
+++ send_email.py
@@ -1,18 +1,21 @@
-import smtplib, ssl
+import smtplib
+import ssl
 
+#it is a email library  which crestes email client sessions
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
-
-    username = "umeshroutgol.com"
-    password = "bavbahyledfzkekw"
-
+    username = "umeshroutgol@gmail.com"
+    password = "ycjdvxfkadyizast"
     receiver = "umeshroutgol@gmail.com"
     context = ssl.create_default_context()
 
-    context=ssl.create_default_context()
+
+    context = ssl.create_default_context()
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
         server.sendmail(username, receiver, message)
 
+
+send_email("message")
